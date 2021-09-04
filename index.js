327dc9b9c2ab7bddc49059b79916405f918bdcc6
Jaya <jaya.vedantam@gmail.com>
2021-09-04 13:53:30 -0700

    add engineer, add team and add manager

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const jest = require("jest");
 // Constructorsconst Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Employee = require('./lib/Employee');
-
+const Engineer = require('./lib/Engineer');
 
 let teamArray = [];
-
+let idArray = [];
+// initialize app, function to add manager
 function initApp() {
   inquirer
     .prompt([
@@ -23,7 +24,43 @@ function initApp() {
           }
           return "Please enter the team's manager's name.";
         }
-      }
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What's the manager's ID?",
+        validate: answer => {
+            if (answer !== "") {
+                return true;
+            }
+            return "Please enter a valid Manager's ID.";
+        }
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What's the manager's email?",
+        validate: answer => {
+            if (answer !== "") {
+                return true;
+            }
+            return "Email address can't be empty.";
+        }
+    },
+    {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What's the manager's office number? (format: 111111111)",
+        validate: answer => {
+            const pass = answer.match(
+                /^[1-9]\d*$/
+            );
+            if (pass) {
+                return true;
+            }
+            return "Please enter a correct phone number.";
+        }
+    }
     ])
     .then((answers) => {
       const manager = new Manager(
@@ -34,8 +71,94 @@ function initApp() {
       );
       teamArray.push(manager);
       console.log("team array ", answers);
-      generateHTML();
+      addTeam();
     });
+
+    //function to add Team after Manager
+
+
+     function addTeam() {
+      inquirer.prompt([
+          {
+              type: "list",
+              name: "memberChoice",
+              message: "What would you like to add next?",
+              choices: [
+                  "Engineer",
+                  "Intern",
+                  "End application"
+              ]
+          }
+      ]).then(userChoice => {
+          switch (userChoice.memberChoice) {
+              case "Engineer":
+                  addEngineer();
+                  break;
+              case "Intern":
+                  addIntern();
+                  break;
+              default:
+                  generateHTML();
+          }
+      });
+  }
+
+
+  function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What's the engineer's name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Engineer's name can't be left empty.";
+            }
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What's the engineer's id?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter a valid Engineer's ID.";
+            }
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What's the engineer's email?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Email address can't be empty.";
+            }
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What's the engineer's GitHub username?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter the engineer's GitHub username.";
+            }
+        }
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+        teamArray.push(engineer);
+        idArray.push(answers.engineerId);
+        addTeam();
+    });
+}
+    
+    
     function generateHTML() {
 
       // Create dist directory for index.html if it doesnt exist
